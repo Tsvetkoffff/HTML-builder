857eddd85908bab6da4320987f90bf052a92747c
Slava Tsvetkov <tsvetkoffff@gmail.com>
2022-05-22 17:53:37 +0300

    feat: solve task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyDir(src, dest) {
+  try {
+    await fs.promises.rm(path.join(__dirname, dest), {
+      recursive: true,
+      force: true,
+    });
+    await fs.promises.mkdir(path.join(__dirname, dest), {
+      recursive: true,
+    });
+
+    const files = await fs.promises.readdir(path.resolve(__dirname, src));
+
+    for (const file of files) {
+      const srcPath = path.join(__dirname, src, file);
+      const destPath = path.join(__dirname, dest, file);
+      await fs.promises.copyFile(srcPath, destPath);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyDir('files', 'files-copy');
