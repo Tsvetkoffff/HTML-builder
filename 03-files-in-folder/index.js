9ec60dbf9e07a0de8221e3378468899bc3048271
Slava Tsvetkov <tsvetkoffff@gmail.com>
2022-05-22 15:39:05 +0300

    feat: solve task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+async function getFilesInfo(folder) {
+  try {
+    const files = await fs.promises.readdir(path.resolve(__dirname, folder), {
+      withFileTypes: true,
+    });
+    for (const file of files) {
+      if (!file.isDirectory()) {
+        const fullName = file.name;
+        const filePath = path.join(__dirname, folder, fullName);
+        const fileName = fullName.split('.')[0];
+        const fileExt = path.extname(fullName).split('.')[1];
+        const fileSize = await fs.promises.stat(filePath).then((f) => f.size);
+
+        console.log(`${fileName} - ${fileExt} - ${fileSize} kb`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+getFilesInfo('secret-folder');
